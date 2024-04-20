4e7a23ac1029ab0ac23a3bad9d8deeffb7dd2886
Shalini2510 <166798782+Shalini2510@users.noreply.github.com>
2024-04-21 01:20:49 +0530

    Shape

---
+++ JAVA/DAY 4,5/Shapes/Shape.java
@@ -0,0 +1,16 @@
+/*
+Task 2: Inheritance
+Create a Shape class with a method area() and extend it with Circle and Rectangle classes overriding the area() method appropriately.
+*/
+
+package assignment;
+
+
+public class Shape {
+    public double area() {
+        return 0; // Default implementation, overridden by subclasses
+    }
+}
+
+
+

3a82850d1b8f0d130c71bc98407bc6d0868c3ac1
Shalini2510 <166798782+Shalini2510@users.noreply.github.com>
2024-04-20 23:58:27 +0530

    Task1

---
+++ JAVA/DAY1,2,3/PrimeNumber.java
@@ -0,0 +1,35 @@
+package assignment;
+import java.util.Scanner;
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		        Scanner scanner = new Scanner(System.in);
+		        System.out.print("Enter an integer: ");
+		        int number = scanner.nextInt();
+		        
+		        
+		        boolean isPrime = true;
+		        
+		        if (number <= 1) {
+		            isPrime = false;
+		        } else {
+		            for (int i = 2; i <= Math.sqrt(number); i++) {
+		                if (number % i == 0) {
+		                    isPrime = false;
+		                    break;
+		                }
+		            }
+		        }
+		        
+		        if (isPrime) {
+		            System.out.println(number + " is a prime number.");
+		        } else {
+		            System.out.println(number + " is not a prime number.");
+		        }
+
+
+	}
+
+}
